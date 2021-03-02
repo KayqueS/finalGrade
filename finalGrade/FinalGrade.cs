086709848243b52a00711a7c9993dfe75ebf7538
KayqueS <kayque.sanmartin65@gmail.com>
2021-03-02 20:43:11 -0300

    Add project files.

---
+++ finalGrade/FinalGrade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace finalGrade
+{
+    class FinalGrade
+    {
+        public string Name;
+        public double Quarter1, Quarter2, Quarter3;
+
+        public double Final()
+        {
+            return Quarter1 + Quarter2 + Quarter3;
+        }
+
+        public bool Approved()
+        {
+            if (Final() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double RemainingNote()
+        {
+            if (Approved())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - Final();
+            }
+        }
+    }
+}
---
+++ finalGrade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace finalGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FinalGrade x = new FinalGrade();
+
+            Console.Write("Student's name: ");
+            x.Name = Console.ReadLine();
+            Console.WriteLine("Enter the student's three grades:");
+            x.Quarter1 = double.Parse(Console.ReadLine());
+            x.Quarter2 = double.Parse(Console.ReadLine());
+            x.Quarter3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"FINAL GRADE = {x.Final():F2}");
+
+            if (x.Approved())
+            {
+                Console.WriteLine("APRPOVED");
+            }
+            else
+            {
+                Console.WriteLine("DISAPPROVED");
+                Console.WriteLine($"MISSED {x.RemainingNote():F2} POINTS");
+            }
+        }
+    }
+}
